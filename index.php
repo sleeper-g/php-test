12d5c8b1457604e8d48b7f08d214d2634e0810a9
sleeper-g <green.sleeper1@gmail.com>
2025-04-01 01:13:53 +0300

    refactoring index.php with task2

--- index.php
+++ index.php
@@ -1,29 +1,22 @@
 #!/usr/bin/env php
 <?php
 echo "Ввод двух чисел: ";
-$userInput = fgets(STDIN);
-$values = explode(" ", trim($userInput));
+$values = explode(" ", trim(fgets(STDIN)));
 
 if (count($values) !== 2) {
     fwrite(STDERR, "Введите, пожалуйста, 2 числа." . PHP_EOL);
     exit();
-}
-
-foreach ($values as $value) {
-    if (!is_numeric($value) || intval($value) != $value) {
-        fwrite(STDERR, "Введите, пожалуйста, число." . PHP_EOL);
-        exit();
-    }
-}
-
-$number1 = intval($values[0]);
-$number2 = intval($values[1]);
-
-if ($number2 === 0) {
+} elseif ($values[1] === 0) {
     fwrite(STDERR, "Делить на 0 нельзя" . PHP_EOL);
     exit();
-}
+};
 
-$result = $number1 / $number2;
-echo $result . PHP_EOL;
+foreach ($values as $value) {
+    if (!is_numeric($value)) {
+        fwrite(STDERR, "Введите, пожалуйста, число." . PHP_EOL);
+        exit();
+    };
+};
+
+fwrite(STDOUT, $values[0] / $values[1] . PHP_EOL);
 ?>
