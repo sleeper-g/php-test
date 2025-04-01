5ab136c2289d22f887a7689b94fb73d03ef537f3
sleeper-g <green.sleeper1@gmail.com>
2025-04-01 10:31:21 +0300

    fix division by 0 and check by input type

--- index.php
+++ index.php
@@ -6,16 +6,18 @@ $values = explode(" ", trim(fgets(STDIN)));
 if (count($values) !== 2) {
     fwrite(STDERR, "Введите, пожалуйста, 2 числа." . PHP_EOL);
     exit();
-} elseif ($values[1] === 0) {
-    fwrite(STDERR, "Делить на 0 нельзя" . PHP_EOL);
-    exit();
-};
+} 
 
 foreach ($values as $value) {
     if (!is_numeric($value)) {
         fwrite(STDERR, "Введите, пожалуйста, число." . PHP_EOL);
         exit();
     };
+};
+
+if (intval($values[1]) === 0) {
+    fwrite(STDERR, "Делить на 0 нельзя" . PHP_EOL);
+    exit();
 };
 
 fwrite(STDOUT, $values[0] / $values[1] . PHP_EOL);
